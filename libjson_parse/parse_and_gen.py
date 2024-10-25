7ce261fb83dfcdd382d8d6831578a41396cd9bf7
Pâris DOUADY <paris.douady@hotmail.fr>
2024-10-25 22:06:53 +0200

    gen unateness and parse more lib files

--- libjson_parse/parse_and_gen.py
+++ libjson_parse/parse_and_gen.py
@@ -1,6 +1,8 @@
 import os
 import json
 import itertools
+from collections import defaultdict
+
 
 def calc_transition(function_str, pins, pin_in, comb_other_pins):
     pin_before = False
@@ -37,11 +39,10 @@ if __name__ == "__main__":
             cell_name = file.split(".json")[0]
             cells[cell_name] = json.loads(f.read())
 
-
     cells_transition_combinations = {}
+    unateness = defaultdict(dict)
 
     for cell, lib in cells.items():
-        print(cell)
         if type (lib) is not dict:
             continue
         for lib_outpin in lib.values():
@@ -50,6 +51,8 @@ if __name__ == "__main__":
             if not "direction" in lib_outpin:
                 continue
             if not lib_outpin["direction"] == "output":
+                continue
+            if not "timing" in lib_outpin:
                 continue
 
             timings = lib_outpin["timing"]
@@ -64,6 +67,28 @@ if __name__ == "__main__":
 
             out_cell = {}
             for pin_in in pins:
+                if "function" not in lib_outpin or lib_outpin["function"] == "IQ" or lib_outpin["function"] == "IQ_N":
+                    out_cell[pin_in] = [
+                        {
+                            "pins": {},
+                            "unate": "positive"
+                        }
+                    ]
+                    continue
+                if len(pins) == 1:
+                    before, after = calc_transition(lib_outpin["function"], pins, pin_in, [])
+                    if before != after:
+                        out_cell[pin_in] = [
+                            {
+                                "pins": {},
+                                "unate": "positive" if before == 0 else "negative"
+                            }
+                        ]
+                        continue
+                    else:
+                        print(f"weird, {cell} {pin_in} has no transition")
+                        continue
+
                 combs = []
                 for combinations in itertools.product([True, False], repeat=len(pins)-1):
                     before, after = calc_transition(lib_outpin["function"], pins, pin_in, combinations)
@@ -77,6 +102,30 @@ if __name__ == "__main__":
                         })
                 out_cell[pin_in] = combs
             cells_transition_combinations[cell] = out_cell
+
+    for cell, pin_combs in cells_transition_combinations.items():
+        shortcell = "_".join(cell.split("__")[1].split("_")[0:-1])
+        for pin, combs in pin_combs.items():
+            positive_unate = False
+            negative_unate = False
+
+            for comb in combs:
+                if comb["unate"] == "positive":
+                    positive_unate = True
+                if comb["unate"] == "negative":
+                    negative_unate = True
+
+            if positive_unate and negative_unate:
+                unateness[shortcell][pin] = "non_unate"
+            elif positive_unate:
+                unateness[shortcell][pin] = "positive_unate"
+            elif negative_unate:
+                unateness[shortcell][pin] = "negative_unate"
+            else:
+                print(f"weird, {shortcell} {pin} has no unate")
+
+    with open('unateness.json', 'w') as f:
+        f.write(json.dumps(unateness, indent=4))
 
     with open("cells_transition_combinations.json", "w") as f:
         f.write(json.dumps(cells_transition_combinations, indent=4))
